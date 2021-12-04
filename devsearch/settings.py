7e301d63fb16a6193656526c76312b488d722cf0
NainishK <34277653+NainishK@users.noreply.github.com>
2021-12-04 14:58:29 +0530

    changed back to staticfiles in settings.py

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -193,7 +193,7 @@ EMAIL_HOST_PASSWORD = 'project.devsearch@123'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
