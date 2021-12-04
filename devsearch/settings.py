83c160c24c617ec41a21fe0ea55c6dbc883068a9
NainishK <34277653+NainishK@users.noreply.github.com>
2021-12-04 15:11:07 +0530

    STATICFILES_DIRS

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -197,9 +197,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
